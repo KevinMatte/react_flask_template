b113b9bd0e7b64240f71ec8f07ddd15d51bed33b
Kevin Matte <matte.kevin@gmail.com>
2019-02-27 11:06:44 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -36,24 +36,28 @@ class App extends Component {
     render() {
         let {model} = this.state;
         return (
-            <div className="max_size">
-                <div className="flexVDisplay max_size">
-                    <h1 className="flexFixed flexAlignCenter">
-                        {model.title || "Loading..."}
-                    </h1>
-                    <div className="flexFixed flexHDisplay max_size">
-                        <div className="flexFixed">
-                            <ul>
-                                <li>Menu item one</li>
-                                <li>Menu item two</li>
-                                <li>Menu item three</li>
-                            </ul>
-                        </div>
-                        <div className="flexHStretched flexAlignMiddle ">
-                            {model.body}
-                        </div>
+            <div className="flexVDisplay max_size">
+                <h1 className="flexFixed flexAlignCenter">
+                    {model.title || "Loading..."}
+                </h1>
+                <div className="flexVStretched flexHDisplay">
+                    <div className="flexFixed">
+                        <ul>
+                            <li>Menu item one</li>
+                            <li>Menu item two</li>
+                            <li>Menu item three</li>
+                        </ul>
+                    </div>
+                    <div className="flexHStretched flexCentered ">
+                        {model.body}
                     </div>
                 </div>
+                <nav className="flexFixed flexHDisplay">
+                    <button className="flexFixed btn btn-primary">Click Me</button>
+                    <div className="flexVStretched">
+                        <input type="text" className="form-control"/>
+                    </div>
+                </nav>
             </div>
         );
     }
